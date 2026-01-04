91fae7d68ea25611ba031dc7768a3f49e1734968
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2026-01-04 18:53:23 +0100

    Init

---
+++ bin/smalizer.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const smalizator = require('../index.js');
+
+smalizator.smalize();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    smalize: () => {
+        console.log('Hello from smalizator!');
+    }
+};
